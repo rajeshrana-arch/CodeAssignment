9a631d2363dfdbf6f61cdebc6af19603cf8b4349
Rajesh Kumar Rana <73537280+rajeshrana-arch@users.noreply.github.com>
2024-04-10 17:34:25 +0530

    added test case for more use cases

--- src/test/java/com/hsbc/assignment/AppTest.java
+++ src/test/java/com/hsbc/assignment/AppTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -35,6 +36,48 @@ public class AppTest
         // Calculate average salary
         Map<String, Double> averageSalaryMap = FindAverageSalaryApp.findAverageSalary(employees);
       // Check if the result matches the expected values
+        assertEquals(expected.size(), averageSalaryMap.size());
+        expected.forEach((key, expectedValue) -> {
+            assertEquals(expectedValue, averageSalaryMap.get(key));
+        });
+    }
+    @Test
+    void testFindAverageSalaryWithBlankSalary() {
+        List<Employee> employees = Arrays.asList();
+        // Calculate average salary
+        Map<String, Double> averageSalaryMap = FindAverageSalaryApp.findAverageSalary(employees);
+        // Check if the result matches the expected values
+        assertTrue(averageSalaryMap.isEmpty());
+    }
+    @Test
+    void testFindAverageSalaryWithSingleEmployee() {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Ashish", "A", "IT", "Pune", "Software Engineer", new Double(10000))
+        );
+        // Calculate average salary
+        Map<String, Double> averageSalaryMap = FindAverageSalaryApp.findAverageSalary(employees);
+        // Expected result
+        Map<String, Double> expected = new HashMap <String, Double>(){{
+            put("Pune,Software Engineer", new Double(10000.0));
+        }};
+        assertEquals(expected.size(), averageSalaryMap.size());
+        expected.forEach((key, expectedValue) -> {
+            assertEquals(expectedValue, averageSalaryMap.get(key));
+        });
+    }
+    @Test
+    void testFindAverageSalaryWithSameGroup() {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Ashish", "A", "IT", "Pune", "Recruiter", new Double(10000)),
+                new Employee("Amit", "R", "HR", "Pune", "Recruiter", new Double(12000)),
+                new Employee("Ramesh", "D", "HR", "Pune", "Recruiter", new Double(14000))
+        );
+        // Calculate average salary
+        Map<String, Double> averageSalaryMap = FindAverageSalaryApp.findAverageSalary(employees);
+        // Expected result
+        Map<String, Double> expected = new HashMap <String, Double>(){{
+            put("Pune,Recruiter", new Double(12000.0));
+        }};
         assertEquals(expected.size(), averageSalaryMap.size());
         expected.forEach((key, expectedValue) -> {
             assertEquals(expectedValue, averageSalaryMap.get(key));
